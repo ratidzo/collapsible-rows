1456e59919a98179eb7e5ca3cbec4fe7a9f637ce
ratidzo <ratygore@gmail.com>
2023-11-05 23:15:08 +0200

    dev: make home page a client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+'use client';
 
 export default function Home() {
 
   return (
-    <></>
-    
+    <div className="p-20">
+      <ul>
+        <li className="border-b">One</li>
+        <li className="border-b"></li>
+        <li className="border-b"></li>
+      </ul>
+    </div>
+
   )
 }
