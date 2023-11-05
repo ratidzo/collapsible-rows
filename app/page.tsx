a98a5bd9daa245a15f6484c2a3f73d66d93417eb
ratidzo <ratygore@gmail.com>
2023-11-05 23:32:51 +0200

    dev: set list margin top

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
   const [items, setItems] = useState(["one", "two", "three"])
 
   return (
-    <div className="p-20">
+    <div className="p-20 text-lg">
       <div className='flex justify-between'>
         <button className='border rounded px-2 py-1'>Remove</button>
         <button className='border rounded px-2 py-1'>Add</button>
       </div>
 
-      <ul>
+      <ul className='mt-8'>
         {items.map((item) => (
           <li key={item} className='border-b'>
             {item}
