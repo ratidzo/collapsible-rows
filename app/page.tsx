0272f2b4b8bd5ed7a61679355fe52f7998315b2b
ratidzo <ratygore@gmail.com>
2023-11-05 23:39:05 +0200

    dev: add handler function addItem

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,23 @@ import { useState } from 'react'
 
 export default function Home() {
   
-  const [items, setItems] = useState(["one", "two", "three"])
+  const [items, setItems] = useState([1, 2, 3])
+
+  function addItem() {
+    setItems((items) => [...items, items.length + 1])
+  }
 
   return (
     <div className="p-20 text-lg">
       <div className='flex justify-between'>
         <button className='border rounded px-2 py-1'>Remove</button>
-        <button className='border rounded px-2 py-1'>Add</button>
+        <button onClick={addItem} className='border rounded px-2 py-1'>Add</button>
       </div>
 
       <ul className='mt-8'>
         {items.map((item) => (
           <li key={item} className='border-b'>
-            {item}
+            Item {item}
           </li>
         ))}
       </ul>
