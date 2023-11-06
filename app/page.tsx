fa05f5722835daab5e453abc2db7adec58acdc78
ratidzo <ratygore@gmail.com>
2023-11-06 20:31:48 +0200

    bug-fix: pass in anon func instead of func ref

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,28 @@ export default function Home() {
     setItems((items) => [...items, items.length + 1])
   }
 
+  function removeItem(item: any) {
+    setItems((items) => items.filter((i) => i !== item))
+  }
+
+  
+
   return (
-    <div className="p-20 text-lg">
+    <div className="p-20 text-lg ">
       <div className='flex justify-between'>
-        <button className='border rounded px-2 py-1'>Remove</button>
+
         <button onClick={addItem} className='border rounded px-2 py-1'>Add</button>
       </div>
 
       <ul className='mt-8'>
         {items.map((item) => (
-          <li key={item} className='border-b'>
-            Item {item}
+          <li key={item} className='flex items-center justify-between border-b py-2'>
+            <span>Item {item}</span>
+            <button onClick={() => removeItem(item)}
+             className='border rounded px-2 py-1
+             w-8 h-8 '>
+              &times;
+            </button>
           </li>
         ))}
       </ul>
