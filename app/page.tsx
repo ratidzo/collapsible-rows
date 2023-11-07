0c357e4caa18341e0b4db3a4aa4cf1067b72af9a
ratidzo <ratygore@gmail.com>
2023-11-07 12:50:48 +0200

    dev: set layer animation to trigger immediately

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, motion, useIsPresent } from 'framer-motion'
 import { useState } from 'react'
 
 let x = 3;
@@ -84,7 +84,7 @@ export default function Home() {
 }
 
 function TR({ item, removeItem }) {
-
+  let isPresent = useIsPresent()
   return (
     <motion.tr
             key={item}
@@ -92,8 +92,10 @@ function TR({ item, removeItem }) {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{
-              opacity: 0,
-              position: "absolute"
+              opacity: 0
+            }}
+            style={{
+              position: isPresent ? "relative" : "absolute"
             }}
             >
               <td>1: item {item}</td>
